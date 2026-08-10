4b1c2cdea7f3c735f75f7e31a27d8e6d8320fa0f
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2026-08-10 13:08:07 +0200

    Add bat wrappers for 3 selected BK7231T and BK7231N flash dumps

---
+++ run_Tuya_1G_Switch_(schemaID-0000026d15)_key7axydcvmea3x9_ven08ot7x16qxbty_WB3S-IPEX_1.0.4_verbose_withBoot.bat
@@ -0,0 +1,11 @@
+@echo off
+set SIM_SCRIPT=%~dp0bk7231_sim.py
+set TARGET_FILE=%~1
+
+if "%TARGET_FILE%"=="" (
+    set TARGET_FILE=%~dp0references\FlashDumps\IoT\BK7231T\Tuya_1G_Switch_(schemaID-0000026d15)_key7axydcvmea3x9_ven08ot7x16qxbty_WB3S-IPEX_1.0.4.bin
+)
+
+echo Running simulator (Verbose) for: %TARGET_FILE%
+python "%SIM_SCRIPT%" "%TARGET_FILE%" --with-boot
+pause
