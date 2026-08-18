c03343d0b085909bd8f47190167257e7b02a739e
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2026-08-19 01:34:26 +0200

    test

---
+++ run_BK7238_QIO_t1u_pci-e_2026-13-6-19-42-48_verbose_withBoot.bat
@@ -0,0 +1,11 @@
+@echo off
+set "SIM_SCRIPT=%~dp0src\main.py"
+set TARGET_FILE=%~1
+
+if "%TARGET_FILE%"=="" (
+    set "TARGET_FILE=%~dp0references\FlashDumps\IoT\BK7238\BK7238_QIO_t1u_pci-e_PC_card_2_2026-13-6-19-42-48.bin"
+)
+
+echo Running simulator (Verbose) for: %TARGET_FILE%
+python "%SIM_SCRIPT%" "%TARGET_FILE%" --with-boot -chip BK7238
+pause
