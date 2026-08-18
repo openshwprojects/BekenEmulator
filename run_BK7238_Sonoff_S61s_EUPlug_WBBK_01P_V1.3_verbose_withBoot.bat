ff1b3e94fea6a5a325e6dfe204fc1de4587a74b0
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2026-08-19 01:13:20 +0200

    bats

---
+++ run_BK7238_Sonoff_S61s_EUPlug_WBBK_01P_V1.3_verbose_withBoot.bat
@@ -0,0 +1,11 @@
+@echo off
+set "SIM_SCRIPT=%~dp0src\main.py"
+set TARGET_FILE=%~1
+
+if "%TARGET_FILE%"=="" (
+    set "TARGET_FILE=%~dp0references\FlashDumps\IoT\BK7238\Sonoff_S61s_EUPlug_WBBK_01P_V1.3.bin"
+)
+
+echo Running simulator (Verbose) for: %TARGET_FILE%
+python "%SIM_SCRIPT%" "%TARGET_FILE%" --with-boot -chip BK7238
+pause
---
+++ run_BK7252N_Cam_7252n_2025-28-9-17-10-56_verbose_withBoot.bat
@@ -0,0 +1,11 @@
+@echo off
+set "SIM_SCRIPT=%~dp0src\main.py"
+set TARGET_FILE=%~1
+
+if "%TARGET_FILE%"=="" (
+    set "TARGET_FILE=%~dp0references\FlashDumps\IoT\BK7252N\Cam_readResult_BK7252N_QIO_7252n_2025-28-9-17-10-56.bin"
+)
+
+echo Running simulator (Verbose) for: %TARGET_FILE%
+python "%SIM_SCRIPT%" "%TARGET_FILE%" --with-boot -chip BK7252N
+pause
